272de5af62dcbc635909b92002f0d19dd004aba7
Fahim <fahimshahariar.fs@gmail.com>
2024-11-11 09:54:19 +0600

    cities controller

---
+++ WebApplication1/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+
+        public JsonResult GetCities()
+        {
+
+            return new JsonResult(
+
+                new List<object> {
+
+                    new { id = 1 , name = "Dhaka" },
+                    new { id = 2 , name = "Rajshahi" }
+
+                });
+
+        }
+
+    }
+}
